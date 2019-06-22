0ee7b4b74614a03ae665ad0a867604985c8a776f
NaskoVasilev <nasko_vasilev@abv.bg>
2019-06-22 12:53:01 +0300

    Razor view engine

--- TestApp/TestApp/Controllers/HomeController.cs
+++ TestApp/TestApp/Controllers/HomeController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using TestApp.Models;
 
 namespace TestApp.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly IConfiguration configuration;
+
+		public HomeController(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+		}
+
 		public IActionResult Index()
 		{
 			return this.View();
@@ -17,6 +28,8 @@ namespace TestApp.Controllers
 
 		public IActionResult Privacy()
 		{
+			ViewBag.People = new List<IdentityUser>() { new IdentityUser{ UserName = "Atanas", Email = "nasko.it.admin@dev.bg" } };
+
 			return View();
 		}
 

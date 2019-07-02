17ee3a134aa5b54795a7836efc3e932356e58b08
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-03 00:34:35 +0300

    Identity configuration

---
+++ Eventures/Eventures/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Eventures.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Eventures.Areas.Identity.Pages.Account
+{
+	[AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LogoutModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+			await _signInManager.SignOutAsync();
+			return Redirect("/");
+		}
+    }
+}

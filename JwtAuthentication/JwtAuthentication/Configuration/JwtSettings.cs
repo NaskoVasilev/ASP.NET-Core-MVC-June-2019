9890fa06b2f51039a9a3a2a74977a20736c38476
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-07 20:54:54 +0300

    Configure Jwt settings and add all needed services in ioc

---
+++ JwtAuthentication/JwtAuthentication/Configuration/JwtSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace JwtAuthentication.Configuration
+{
+	public class JwtSettings
+	{
+		public string Secret { get; set; }
+	}
+}
--- JwtAuthentication/JwtAuthentication/Startup.cs
+++ JwtAuthentication/JwtAuthentication/Startup.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Text;
+using JwtAuthentication.Configuration;
 using JwtAuthentication.Data;
 using JwtAuthentication.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JwtAuthentication
 {
@@ -36,6 +33,29 @@ namespace JwtAuthentication
 			services.AddIdentity<ApplicationUser, IdentityRole>()
 					.AddDefaultTokenProviders()
 					.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			var jwtSettingsSection = Configuration.GetSection("JwtSettings");
+			services.Configure<JwtSettings>(jwtSettingsSection);
+
+			var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
+			var key = Encoding.UTF8.GetBytes(jwtSettings.Secret);
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(options =>
+			{
+				options.RequireHttpsMetadata = false;
+				options.SaveToken = true;
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(key),
+					ValidateIssuer = false,
+					ValidateAudience = false
+				};
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +71,7 @@ namespace JwtAuthentication
 				app.UseHsts();
 			}
 
+			app.UseAuthentication();
 			app.UseHttpsRedirection();
 			app.UseMvc();
 		}

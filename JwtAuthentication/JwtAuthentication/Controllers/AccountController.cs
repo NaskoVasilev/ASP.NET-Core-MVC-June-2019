cb9e7a167832c2e5b4018dd8f69861b3d43920a0
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-07 21:29:23 +0300

    Implement user registration logic, add migration and created login and register input models with needed validation

--- JwtAuthentication/JwtAuthentication/Controllers/AccountController.cs
+++ JwtAuthentication/JwtAuthentication/Controllers/AccountController.cs
@@ -1,6 +1,43 @@
-﻿namespace JwtAuthentication.Controllers
+﻿using JwtAuthentication.InputModels.Account;
+using JwtAuthentication.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtAuthentication.Controllers
 {
 	public class AccountController : ApiController
 	{
+		private readonly UserManager<ApplicationUser> userManager;
+
+		public AccountController(UserManager<ApplicationUser> userManager)
+		{
+			this.userManager = userManager;
+		}
+
+		[HttpPost("[action]")]
+		public async Task<ActionResult<ApplicationUser>> Register(RegisterInputModel model)
+		{
+			ApplicationUser user = new ApplicationUser
+			{
+				Email = model.Email,
+				UserName = model.Username,
+				FullName = model.FullName
+			};
+
+			var result = await userManager.CreateAsync(user);
+			if(!result.Succeeded)
+			{
+				return BadRequest(result.Errors.Select(e => e.Description).ToList());
+			}
+			return user;
+		}
+
+		[HttpPost("[action]")]
+		public async Task<ActionResult<ApplicationUser>> Login(LoginInputModel model)
+		{
+			return null;
+		}
 	}
 }

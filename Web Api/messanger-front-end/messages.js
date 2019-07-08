16fe9517d3b524346b94bdceedd3cffa8a7a72d9
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-08 22:15:27 +0300

    Messenger app front-end extending - add user login and register forms and logged-in navbar and ajax calls for login and register api endpoints

---
+++ Web Api/messanger-front-end/messages.js
@@ -0,0 +1,45 @@
+function loadMessages() {
+    $.get(apiUrl + '/messages/all')
+        .done(data => {
+            renderMessages(data)
+        })
+        .fail(error => {
+            console.log(error);
+        })
+}
+
+function createMessage() {
+    if (!user) {
+        alert('You have to choose username before send message.')
+        return;
+    }
+
+    let content = $('#message').val();
+    if (!content) {
+        alert('You can not send message without content.')
+        return;
+    }
+
+    $.post({
+        url: apiUrl + '/messages/create',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify({ content: content, user: user })
+    })
+        .done(() => {
+            loadMessages();
+        })
+        .fail(error => {
+            console.log(error);
+        });
+}
+
+function renderMessages(data) {
+    let messagesHolder = $('#messages');
+    messagesHolder.empty();
+    for (let message of data) {
+        let newMessage = $(`<div class="message d-flex justify-content-start"><strong>${message.user}</strong>: ${message.content}</div>`)
+        messagesHolder.append(newMessage);
+    }
+}

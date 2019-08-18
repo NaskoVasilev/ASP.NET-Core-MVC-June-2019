a3e55ccb913769059dcc57c68989441d06806e36
NaskoVasilev <nasko_vasilev@abv.bg>
2019-08-18 19:08:33 +0300

    Restore client side libraires using Libman, and create using signlR receive and send message in real time, create ChatHub

---
+++ SignalR/Chat/Chat/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Chat.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Chat.Hubs
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        private readonly IMessageService messageService;
+
+        public ChatHub(IMessageService messageService)
+        {
+            this.messageService = messageService;
+        }
+
+        public async Task SendMessage(string message, string recipientId)
+        {
+            string userId = Context.UserIdentifier;
+            string username = Context.User.Identity.Name;
+
+            await messageService.Create(message, userId, recipientId);
+
+            await Clients.User(recipientId).SendAsync("ReceiveMessage", message, userId);
+        }
+    }
+}
